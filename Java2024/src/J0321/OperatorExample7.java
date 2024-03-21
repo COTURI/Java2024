db7bfa411fb8fc3b4daf1f7f4433843bed6e7fdc
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-21 16:31:02 +0900

    go

---
+++ Java2024/src/J0321/OperatorExample7.java
@@ -0,0 +1,19 @@
+package J0321;
+
+public class OperatorExample7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 10;
+		int b = 10;
+		b = ~a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		b= ~b;
+		
+		System.out.println(b);
+	}
+
+}
