160272f5b7183058e55fe586c0d6bb54d4f4d3ec
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-03 12:25:18 +0900

    g

---
+++ Java2024/src/J04034/p7/Member.java
@@ -0,0 +1,9 @@
+package J04034.p7;
+
+public class Member {
+	public String id;
+
+	public Member(String id) {
+		this.id = id;
+	}
+}
---
+++ Java2024/src/J04034/p9/Key.java
@@ -0,0 +1,9 @@
+package J04034.p9;
+
+public class Key {
+	public int number;
+	
+	public Key(int number) {
+		this.number = number;
+	}
+}
