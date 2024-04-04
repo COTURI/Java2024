8078d4da5b0b0e5bd9ab0ada7d05b2d6bfc6b9e8
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-04 17:47:54 +0900

    g

--- Java2024/src/J0404/p64/baseball.java
+++ Java2024/src/J0404/p64/baseball.java
@@ -27,24 +27,28 @@ public class baseball {
 		for (int b = 0; b < 6; b++) {
 			System.out.print(bbg[b] + " ");
 		}
-		
-		
+
 		int[] answer = new int[3];
+		int str = 0;
+		int ball = 0;
 		while (true) {
 			for (int i = 0; i < answer.length; i++) {
 				answer[i] = sc.nextInt();
 			}
 			System.out.println("Input number...");
 			System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
-			
-			
+			for (int i = 0; i < answer.length; i++) {
+				if (answer[i] == bbg[i]) {
+					str++;
+				}
+			}
+
 			if (Arrays.equals(answer, bbg)) {
 				System.out.println("strike : 3, ball : 0");
 				System.out.println("Game Over!!!");
 				break;
-			}else if() {
-				System.out.println("strike : 2, ball : 1");
 
+			}
 		}
 	}
 }
