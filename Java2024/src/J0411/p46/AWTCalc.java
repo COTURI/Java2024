259c98f5c23e03ae53a7b9aff3a3c727354e7ba7
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-12 17:17:17 +0900

    0412

---
+++ Java2024/src/J0411/p46/AWTCalc.java
@@ -0,0 +1,21 @@
+package J0411.p46;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionListener;
+
+public class AWTCalc implements ActionListener{
+	private Frame f;
+	private TextField tf;
+	private Panel p;
+	private Button[] btnNum;
+	
+	private Button bplus, bMinus,bMul.bDiv.bRes;
+	private int operand = 0;
+	
+	private emim OP{
+		OP_NULL. OP_PLUS, OP_MINUS, OP_MUL,OP_DIV
+	};
+}
