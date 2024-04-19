7a103b9172ac84ee92d660fc622b93a0ab48ec73
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-19 10:47:23 +0900

    0419

---
+++ Java2024/src/log/InfoVo.java
@@ -0,0 +1,18 @@
+package log;
+
+public class InfoVo {
+	private String id;
+	private String pwd;
+	
+	public InfoVo(String id, String pwd) {
+		this.id = id;
+		this.pwd = pwd;
+			}
+
+	public String getId() {
+		return id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+}
